6a74eee41378ebcc9e48c75967bce3c0694b8322
naufal <naufalarkan2004@gmail.com>
2026-09-08 23:43:21 +0700

    create main menu

--- main.py
+++ main.py
@@ -37,20 +37,31 @@ def delete():
 # /===== Main Program =====/
 # Create your main program here
 def main():
-    """Function for main program
-    """
+    while True:
+        print("="*70)
+        print("EMPLOYEE DATA MANAGEMENT SYSTEM")
+        print("="*70)
+        print("1. Add Employee Data")
+        print("2. View Employee Data")
+        print("3. Update Employee Data")
+        print("4. Delete Employee Data")
+        print("5. Exit")
+        print("="*70)
 
-    input_user = input("Insert your option: ")
-    if input_user == "1":
-        read()
-    elif input_user == "2":
-        create()
-    elif input_user == "3":
-        update()
-    elif input_user == "4":
-        delete()
-    else:
-        print("Input is not valid !")
+        input_user = input("Insert your choice: ").strip()
+        if input_user == "1":
+            add_data()
+        elif input_user == "2":
+            view_data()
+        elif input_user == "3":
+            update_data()
+        elif input_user == "4":
+            delete_data()
+        elif input_user == "5":
+            print("\n Thank you for using this program, see you next time!")  
+            break  
+        else:
+            print("[X] Input is not valid !")
 
 
 if __name__ == "__main__":
